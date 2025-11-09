67c3facf08f77cc2108858deec08c355a50088ea
maxmaster <yousseflkashef000@gmail.com>
2025-11-09 14:59:09 +0200

    Implement RCC register definitions and initial SysTick configuration function

---
+++ lib/rcc.c
@@ -0,0 +1,14 @@
+#include "rcc.h"
+
+
+void rcc_set_SysTick(rcc_clktype_t rcc){
+    switch (rcc) {
+        case RCC_CLK_HSI:
+            
+            break;
+        case RCC_CLK_HSE:
+            break;
+        case RCC_CLK_PLL:
+            break;
+    }
+}
