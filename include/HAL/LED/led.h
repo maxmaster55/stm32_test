f95766b0fbac64dbd12c0c6550b5efa857f0f473
maxmaster <yousseflkashef000@gmail.com>
2025-11-15 13:34:57 +0200

    added docs

--- include/HAL/LED/led.h
+++ include/HAL/LED/led.h
@@ -6,9 +6,36 @@ typedef enum {
 } led_return_t;
 
 
-
+/**
+ * @brief Initialize all LEDs based on led_cfg configuration
+ * 
+ * @return led_return_t 
+ */
 led_return_t led_init();
+
+/**
+ * @brief Turn on the specified LED
+ * 
+ * @param name The name of the LED to turn on
+ * @return led_return_t 
+ */
 led_return_t led_Turn_on(led_name_t name);
+
+
+/**
+ * @brief Turn off the specified LED
+ * 
+ * @param name The name of the LED to turn off
+ * @return led_return_t 
+ */
 led_return_t led_Turn_off(led_name_t name);
+
+
+/**
+ * @brief Toggle the specified LED
+ * 
+ * @param name The name of the LED to toggle
+ * @return led_return_t 
+ */
 led_return_t led_Toggle(led_name_t name);
 
