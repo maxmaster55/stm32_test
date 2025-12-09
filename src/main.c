24561a5ebf5ecb8858f5ac67ba2eebf631e1399a
maxmaster <yousseflkashef000@gmail.com>
2025-12-09 21:00:11 +0200

    tested custom char

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <service/scheduler/sched.h>
 #include <MCAL/RCC/rcc.h>
 #include <HAL/C_LCD/lcd.h>
-
+#include <MCAL/systick/systick.h>
 
 #define CLK 16000000
+
+uint8_t heart[8] = {
+    0b00000,
+    0b01010,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,
+    0b00000
+};
 
 lcd_cfg_t lcd_cfg = {
     .port = GPIOA,
@@ -24,8 +35,9 @@ int main(void)
     
     lcd_async_init(&lcd_cfg);
     char* x = "Touch some grass";
-    lcd_async_write_str(&lcd_cfg, "a");
-
+    systick_wait(500);
+    lcd_async_save_custom_char(&lcd_cfg, &heart, 0);
+    lcd_async_write_custom_char(&lcd_cfg, 0);
     sched_start();
 
     // lcd_init(&lcd_cfg);
