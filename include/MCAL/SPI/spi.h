6d397c927a3349be6cfa3637519f18abf950253a
maxmaster <yousseflkashef000@gmail.com>
2025-12-23 13:56:20 +0200

    added spi reg defines

---
+++ include/MCAL/SPI/spi.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "spi_regs.h"
+
+// APB2
+#define SPI4 ((SPI_regs_t *)0x40013400)
+#define SPI1 ((SPI_regs_t *)0x40013000)
+// APB1
+#define SPI3 ((SPI_regs_t *)0x40003C00)
+#define SPI2 ((SPI_regs_t *)0x40003800)
+
+
+
+
+
