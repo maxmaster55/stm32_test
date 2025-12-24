fe83f186c4ea4d1dfb4f16d8edd4eae306266f9c
maxmaster <yousseflkashef000@gmail.com>
2025-12-24 23:22:14 +0200

    tested WD

--- src/main.c
+++ src/main.c
@@ -4,15 +4,32 @@
 #include <MCAL/WDT/watch_dog.h>
 
 
+GPIO_PinConfig_t pin = {
+    .port = GPIOC,
+    .pin = 13,
+    .mode = GPIO_MODE_OUTPUT,
+    .pull = GPIO_PULL_NO,
+    .speed = GPIO_SPEED_HIGH,
+    .output_type = GPIO_OUTPUT_PUSHPULL
+};
 
 
+int counter = 0;
 int main(void)
-{
+{    
+    rcc_En_clk_preiph(RCC_GPIOC);  
+    gpio_init(&pin);
+
+
     rcc_enable_LSI();
     wd_init(10000, WD_PRESCALER_DIV32);
     
     while (1)
     {
-        
+    if (counter == 50000)
+    {
+        gpio_write(GPIOC,13, 1);
+    }
+    counter += 1;
     }
 }
