236e5edd57e39126e1562335b6ef996fca68fad8
maxmaster <yousseflkashef000@gmail.com>
2025-12-25 01:58:57 +0200

    added spi options

--- include/HAL/HSerial/hserial.h
+++ include/HAL/HSerial/hserial.h
@@ -2,6 +2,7 @@
 
 #include "glob.h"
 #include <MCAL/uart/uart.h>
+#include <MCAL/SPI/spi.h>
 #include <MCAL/DMA/dma.h>
 
 
@@ -29,8 +30,13 @@ typedef struct {
     uint32_t  tx_length;
 } dma_ctx_t;
 
+
+typedef void (*HSerial_callback_t)(void);
+
 typedef struct {
     HSerial_type_t type;
+    HSerial_callback_t rx_callback;
+    HSerial_callback_t tx_callback;
     dma_ctx_t _dma;
     union {
         struct {
@@ -39,12 +45,13 @@ typedef struct {
             uart_word_length_t word_length;
             uart_parity_t parity;
             uart_stop_bits_t stop_bits;
-            uart_callback_t rx_callback;
-            uart_callback_t tx_callback;
+
         } uart_cfg;
-        // struct {
-        //     
-        // } spi_cfg;
+        struct {
+            spi_num_t spi_num;
+            spi_mode_t mode;
+            spi_speed_t speed;
+        } spi_cfg;
     };
 
 }HSerial_instance_t;
